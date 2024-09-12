5e730a2ad014330d7186b5972812b646bf4dc7fe
onidlareg <ggnetom@gmail.com>
2024-09-11 22:22:29 -0300

    application run

--- src/main/java/br/edu/imepac/domain/Aluno.java
+++ src/main/java/br/edu/imepac/domain/Aluno.java
@@ -7,4 +7,28 @@ public class Aluno {
     
     private String email;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
